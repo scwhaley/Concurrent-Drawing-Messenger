d4df351660cb3e8688f8f4790d284193b84b865d
Spencer <26808399+scwhaley@users.noreply.github.com>
2021-04-15 08:26:56 -0700

    Added SVG background, top level CSS reset, and tried debugging why i get faults on iOS Safari

--- SpringServer/src/main/java/com/example/demo/Security/SecurityConfig.java
+++ SpringServer/src/main/java/com/example/demo/Security/SecurityConfig.java
@@ -58,7 +58,6 @@ import org.springframework.web.filter.CorsFilter;
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
         configuration.addAllowedOrigin("http://localhost:3000");
-        configuration.addAllowedOrigin("http://192.168.0.11:3000");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.addExposedHeader("Authorization");
