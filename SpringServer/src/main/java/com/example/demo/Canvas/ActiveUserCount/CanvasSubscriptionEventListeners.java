09284dfd3cc689feea90888cbeedb48442ec5bb8
Spencer <26808399+scwhaley@users.noreply.github.com>
2021-06-05 19:36:07 -0700

    Fixed SQL exception on subscription event listener by adding @Modifying and @Transactional annotations to the custom repository Query. Need to find alternative way to keep track of unsubscriptions since STOMP UNSUBSCRIBE frames do not include the destination of the unsubscription.

--- SpringServer/src/main/java/com/example/demo/Canvas/ActiveUserCount/CanvasSubscriptionEventListeners.java
+++ SpringServer/src/main/java/com/example/demo/Canvas/ActiveUserCount/CanvasSubscriptionEventListeners.java
@@ -7,9 +7,9 @@ import com.example.demo.DemoApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
 
@@ -17,25 +17,17 @@ import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
 public class CanvasSubscriptionEventListeners {
     
     private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
-
     private CanvasUserCountRepo canvasUserCountRepo;
 
     public CanvasSubscriptionEventListeners(CanvasUserCountRepo canvasUserCountRepo) {
         this.canvasUserCountRepo = canvasUserCountRepo;
     }
 
-    @EventListener(SessionConnectEvent.class)
-	public void handleWSConnectListener (SessionConnectEvent event){
-		logger.info("Recieved WS Session Connect Event");
-	}
-
 	@EventListener(SessionSubscribeEvent.class)
 	public void handleWSSubscribeListener (SessionSubscribeEvent event){
-		StompHeaderAccessor headers = StompHeaderAccessor.wrap(event.getMessage());
-        String destination = headers.getDestination();
-        String canvasID = destination.substring(destination.lastIndexOf("/")+1);
-		logger.info("Recieved WS Session Subscribe Event to cavnasID: " + canvasID);
+		String canvasID = extractCanvasID(event.getMessage());
 
+		logger.info("Recieved WS Session Subscribe Event to canvasID: " + canvasID);
         logger.info("User count before Subscribe Event = " + getNumberOfActiveUsers(Integer.decode(canvasID)));
 
         canvasUserCountRepo.incrementActiveUserCount(Integer.decode(canvasID));
@@ -43,13 +35,13 @@ public class CanvasSubscriptionEventListeners {
         logger.info("User count after Subscribe Event = " + getNumberOfActiveUsers(Integer.decode(canvasID)));
 	}
 
+    //This does not work as written since the UNSUBSCRIBE frame does not unsubscribe to a destination, it unsubsribers via ID of the subscription.
+    //This ID is specific to the connection (what does connectino exactly mean?). So we must find some way to correlate the ID to the destination.
     @EventListener(SessionUnsubscribeEvent.class)
 	public void handleWSUnsubscribeListener (SessionUnsubscribeEvent event){
-		StompHeaderAccessor headers = StompHeaderAccessor.wrap(event.getMessage());
-        String destination = headers.getDestination();
-        String canvasID = destination.substring(destination.lastIndexOf("/")+1);
-		logger.info("Recieved WS Session unsubscribe Event to cavnasID: " + canvasID);
+        String canvasID = extractCanvasID(event.getMessage());
 
+		logger.info("Recieved WS Session unsubscribe Event to canvasID: " + canvasID);
         logger.info("User count before unsubscribe Event = " + getNumberOfActiveUsers(Integer.decode(canvasID)));
 
         canvasUserCountRepo.decrementActiveUserCount(Integer.decode(canvasID));
@@ -68,5 +60,11 @@ public class CanvasSubscriptionEventListeners {
         }
 
         return numActiveUsers;
+    }
+
+    private String extractCanvasID(Message<byte[]> message){
+        StompHeaderAccessor headers = StompHeaderAccessor.wrap(message);
+        String destination = headers.getDestination();
+        return destination.substring(destination.lastIndexOf("/")+1);
     }
 }
--- SpringServer/src/main/java/com/example/demo/Canvas/ActiveUserCount/CanvasUserCountRepo.java
+++ SpringServer/src/main/java/com/example/demo/Canvas/ActiveUserCount/CanvasUserCountRepo.java
@@ -1,19 +1,27 @@
 package com.example.demo.Canvas.ActiveUserCount;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CanvasUserCountRepo extends CrudRepository<CanvasUserCount, Integer> {
     
     //This inserts a new row, but if it alredy exists for the specified canvas, increments the number of active users
+    //Because this method uses the INSERT and UPDATE statements, we must use the @Modifying annotation. Since these statements
+    //are meant to change the data in the database, the persistence context needs to be modified as well.
+    //The @Transactional annotation is required to indicat
+    @Modifying
+    @Transactional
     @Query(value = "INSERT INTO canvas_user_count (canvas_id, active_users) VALUES(:canvas_ID,1) ON CONFLICT(canvas_id) DO UPDATE SET active_users = canvas_user_count.active_users + 1",
     nativeQuery = true)
     public void incrementActiveUserCount(@Param("canvas_ID") Integer canvas_ID);
 
-    @Query(value = "UPDATE canvas_user_count SET active_users = canvas_user_count.active_users - 1 WHERE canvas_ID = :canvas_ID",
+    @Modifying
+    @Query(value = "UPDATE canvas_user_count SET active_users = canvas_user_count.active_users - 1 WHERE canvas_id = :canvas_ID",
     nativeQuery = true)
     public void decrementActiveUserCount(@Param("canvas_ID") Integer canvas_ID);
 }
